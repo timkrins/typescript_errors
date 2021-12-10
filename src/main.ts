4afd9d4e7f1cea275134c5cfb81917a948fffef4
Tim Krins <timkrins@gmail.com>
2021-12-10 13:52:09 +0000

    Logging

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,11 @@ import winston from 'winston';
 
 const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.combine(winston.format.colorize(), winston.format.json()),
-  transports: [new winston.transports.Console({ format: winston.format.simple() })],
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
+    }),
+  ],
   handleExceptions: true,
 });
 
